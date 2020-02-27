d79698399d4efb226642ead667fc4b955b2111ef
smartlitchi <smartlitchi@pm.me>
2020-02-27 11:25:14 +0100

    manual way of entering books into the log finished

---
+++ reading_log.py
@@ -0,0 +1,34 @@
+import json
+import jinja2
+from dateutil.parser import parse
+
+def get_books(json_file):
+    with open(json_file, "r") as read_file:
+        log = json.load(read_file)
+    return log["books"]
+
+def generate_html(books_list):
+    file_loader = jinja2.FileSystemLoader('templates')
+    env = jinja2.Environment(loader=file_loader)
+    template = env.get_template('monthly_log.html')
+    return(template.render(months=books_list))
+
+def sort_by_date(books_list):
+    epoch = parse(books[0]["date"])
+    monthly_books = list()
+    reading_list = list()
+    for book in books_list:
+        if parse(book["date"]) == epoch:
+            monthly_books.append(book)
+        else:
+            epoch = parse(book["date"])
+            reading_list.append(monthly_books)
+            monthly_books = [book]
+    reading_list.append(monthly_books)
+    return reading_list
+
+books = get_books("assets/reading_log.json")
+books_sorted = sort_by_date(books)
+
+with open('render/reading_log.html', 'w') as webpage:
+    webpage.write(generate_html(books_sorted))
