a04c725d27f592e698dd4cae9fab0759c4d8d71d
zimhat <zimhat@pm.me>
2020-03-11 18:16:55 +0100

    docstrings added to web_render

--- web_render.py
+++ web_render.py
@@ -1,12 +1,22 @@
 import jinja2
 
 def generate_html(books_list):
+    """
+    Takes a list of books and generate the html template
+
+    books_list -- list of all the books I need to render
+    """
     file_loader = jinja2.FileSystemLoader('templates')
     env = jinja2.Environment(loader=file_loader)
     template = env.get_template('monthly_log.html')
     return(template.render(months=books_list))
 
 def render_html(books):
+    """
+    Writes the template to a webpage
+
+    books -- list of all the books I need to render
+    """
     with open('render/reading_log.html', 'w') as webpage:
         webpage.write(generate_html(books))
         print("webpage successfully rendered")
