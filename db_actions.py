c8242c32ff52e9c24f6f555355f1e2f8bc47f056
smartlitchi <smartlitchi@pm.me>
2020-03-11 18:07:01 +0100

    docstrings added to db_actions

--- db_actions.py
+++ db_actions.py
@@ -2,11 +2,21 @@ import json
 from dateutil.parser import parse
 
 def get_books(json_file):
+    """
+    Reads the json database
+
+    json_file -- str, the path of the database
+    """
     with open(json_file, "r") as read_file:
         log = json.load(read_file)
     return log["books"]
 
 def sort_by_date(books_list):
+    """
+    Returns a sorted list of books. The sorting is in reverse chronological order
+
+    books_list -- a list of books
+    """
     epoch = parse(books_list[0]["date"])
     monthly_books = list()
     reading_list = list()
@@ -21,6 +31,11 @@ def sort_by_date(books_list):
     return reading_list
 
 def parse_json(json_file):
+    """
+    Returns the list of all books
+
+    json_file -- string of json database's path
+    """
     books = get_books(json_file)
     books_sorted = sort_by_date(books)
     return books_sorted
