d9999e683359293c4f4c4a5fc393efb018f71b52
zimhat <zimhat@pm.me>
2020-03-13 09:11:21 +0100

    no more hardcoded path in web_render

--- reading_log.py
+++ reading_log.py
@@ -5,7 +5,9 @@ import web_render
 if __name__ == "__main__":
     monthly_log = 'assets/monthly_reports/mar2020.txt'
     json_file = 'assets/reading_log.json'
+    template_path = 'templates/monthly_log.html'
+    webpage_path = 'render/reading_log.html'
     books_of_the_month = new_books.parse_new_books(monthly_log)
     #db_actions.add_new_books(json_file, books_of_the_month)
     books_parsed = db_actions.get_monthly_lists(json_file)
-    web_render.render_html(books_parsed)
+    web_render.render_html(books_parsed, template_path, webpage_path)
--- web_render.py
+++ web_render.py
@@ -1,22 +1,26 @@
 import jinja2
 
-def generate_html(books_list):
+def generate_html(books_list, template_path):
     """
-    Takes a list of books and generate the html template
+    Takes a list of books and generate the html template as a string
 
-    books_list -- list of all the books I need to render
+    books_list -- list of lists of all the books I need to render, sorted by month
+    template_path -- str, path of the template to be used
     """
-    file_loader = jinja2.FileSystemLoader('templates')
+    template_folder, template_file = template_path.split('/')
+    file_loader = jinja2.FileSystemLoader(template_folder)
     env = jinja2.Environment(loader=file_loader)
-    template = env.get_template('monthly_log.html')
+    template = env.get_template(template_file)
     return(template.render(months=books_list))
 
-def render_html(books):
+def render_html(books, template_path, webpage_path):
     """
     Writes the template to a webpage
 
     books -- list of all the books I need to render
+    template_path -- str, path of the template to be used
+    webpage_path -- str, path for the output
     """
-    with open('render/reading_log.html', 'w') as webpage:
-        webpage.write(generate_html(books))
+    with open(webpage_path, 'w') as webpage:
+        webpage.write(generate_html(books, template_path))
         print("webpage successfully rendered")
