ad5edb68cb0b1f9e4ee4a3702a530929f57ecdbf
zimhat <zimhat@pm.me>
2020-11-04 10:42:55 +0100

    Add init function

--- reading_log.py
+++ reading_log.py
@@ -4,12 +4,16 @@ import web_render
 import sys
 
 if __name__ == "__main__":
+    assets_folder = 'assets/'
     json_file = 'assets/reading_log.json'
     template_path = 'templates/monthly_log.html'
     webpage_path = 'render/index.html'
     if len(sys.argv) == 2:
-        books_of_the_month = new_books.parse_new_books(sys.argv[-1])
-        db_actions.add_new_books(json_file, books_of_the_month)
+        if sys.argv[-1] == '--init':
+            db_actions.init_log(assets_folder, json_file)
+        else:
+            books_of_the_month = new_books.parse_new_books(sys.argv[-1])
+            db_actions.add_new_books(json_file, books_of_the_month)
     books_parsed = db_actions.get_monthly_lists(json_file)
     web_render.render_html(books_parsed, template_path, webpage_path)
     web_render.copy_covers('assets/covers', 'render/covers')
